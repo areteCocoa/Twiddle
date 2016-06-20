3318b1a9e51c6b47e9933775ac6a7065aae7d2a8
thomas-ring <thomasjring@me.com>
2016-06-20 15:01:56 -0400

    Add Twitter and other Frameworks to project

--- Twiddle/AppDelegate.m
+++ Twiddle/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+#import <TwitterKit/TwitterKit.h>
+#import <MoPub/MoPub.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +21,45 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[Twitter sharedInstance] startWithConsumerKey:@"fbmle15DlXrs3MHuh8vk48laC" consumerSecret:@"YH8VJcDuq57Wv3J3oVUMy7ojHCK3VPQwIMPSpGp0oQv1mwklS5"];
+    
+    [Fabric with:@[[Crashlytics class], [Twitter class], [MoPub class]]];
+    
+    NSString * userID = [Twitter sharedInstance].sessionStore.session.userID;
+    TWTRAPIClient * client = [[TWTRAPIClient alloc] initWithUserID: userID];
+    
+    [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
+        if (session) {
+            NSLog(@"signed in as %@", [session userName]);
+        } else {
+            NSLog(@"error: %@", [error localizedDescription]);
+        }
+    }];
+    
+    //
+    NSString *statusesShowEndpoint = @"https://api.twitter.com/1.1/statuses/home_timeline.json";
+    NSDictionary *params = @{};
+    NSError *clientError;
+    
+    NSURLRequest *request = [client URLRequestWithMethod:@"GET" URL:statusesShowEndpoint parameters:params error:&clientError];
+    
+    if (request) {
+        [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            if (data) {
+                // handle the response data e.g.
+                NSError *jsonError;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                NSLog(@"%@", json);
+            }
+            else {
+                NSLog(@"Error: %@", connectionError);
+            }
+        }];
+    }
+    else {
+        NSLog(@"Error: %@", clientError);
+    }
+    
     return YES;
 }
 
---
+++ Twiddle/TimelineTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimelineTableViewController.h
+//  Twiddle
+//
+//  Created by Thomas Ring on 6/19/16.
+//  Copyright © 2016 TRing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineTableViewController : UITableViewController
+
+@end
