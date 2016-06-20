ff9559144082441155abd61d0fe625924e57aec3
thomas-ring <thomasjring@me.com>
2016-06-20 15:38:29 -0400

    Add TweetTextTableViewCell for tweets with only text, no attachments

---
+++ Twiddle/TweetTextTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TweetTextTableViewCell.h
+//  Twiddle
+//
+//  Created by Thomas Ring on 6/20/16.
+//  Copyright © 2016 TRing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetTextTableViewCell : UITableViewCell
+
+@end
---
+++ Twiddle/TweetTextTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TweetTextTableViewCell.m
+//  Twiddle
+//
+//  Created by Thomas Ring on 6/20/16.
+//  Copyright © 2016 TRing. All rights reserved.
+//
+
+#import "TweetTextTableViewCell.h"
+
+@implementation TweetTextTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
