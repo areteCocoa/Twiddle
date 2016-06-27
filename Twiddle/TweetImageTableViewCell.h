bb04d3d3bb4bd19c9bde40cad2c7420bb2a86fff
thomas-ring <thomasjring@me.com>
2016-06-27 07:15:52 -0400

    Add TweetImageTableViewCell for image cells on the timeline, and a squirrel

---
+++ Twiddle/TweetImageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TweetImageTableViewCell.h
+//  Twiddle
+//
+//  Created by Thomas Ring on 6/27/16.
+//  Copyright © 2016 TRing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetImageTableViewCell : UITableViewCell
+
+@end
